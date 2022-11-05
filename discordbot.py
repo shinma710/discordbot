96d02d05be6a14b9f1abb8d80b607b20919ae24b
shinma710 <66122656+shinma710@users.noreply.github.com>
2022-11-05 11:09:45 +0900

    test2

--- discordbot.py
+++ discordbot.py
@@ -27,13 +27,13 @@ async def on_voice_state_update(member, before, after):
 
     if member.guild.id == 825601093802655775 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel(825601094323011625)
+        alert_channel = bot.get_channel(825601094323011625)
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に出勤しました。'
-            await alert_channel.send(msg)
+            await ctx.send(msg)
         elif after.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退勤しました。'
-            await alert_channel.send(msg)
+            await ctx.send(msg)
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
