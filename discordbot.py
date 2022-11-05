7d3517cb3247456b9c4224b28eff0765c3b0cd68
shinma710 <66122656+shinma710@users.noreply.github.com>
2022-11-05 11:29:04 +0900

    本番化

--- discordbot.py
+++ discordbot.py
@@ -25,9 +25,9 @@ async def ping(ctx):
 @bot.event
 async def on_voice_state_update(member, before, after):
 
-    if member.guild.id == 825601093802655775 and (before.channel != after.channel):
+    if member.guild.id == 1034806991031378012 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = bot.get_channel(825601094323011625)
+        alert_channel = bot.get_channel(1034806992537145445)
         if before.channel is None:
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に出勤しました。'
             await alert_channel.send(msg)
