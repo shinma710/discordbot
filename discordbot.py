97aedafbcaba70f42e3d026748fbc6b679581d1d
shinma710 <66122656+shinma710@users.noreply.github.com>
2022-11-05 10:59:17 +0900

    add test

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ import discord
 import traceback
 from discord.ext import commands
 from os import getenv
+from datetime import datetime, timedelta
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,7 +20,20 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+# チャンネル入退室時の通知処理
+@client.event
+async def on_voice_state_update(member, before, after):
 
+    if member.guild.id == 825601093802655775 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(825601094323011625)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に出勤しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退勤しました。'
+            await alert_channel.send(msg)
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
